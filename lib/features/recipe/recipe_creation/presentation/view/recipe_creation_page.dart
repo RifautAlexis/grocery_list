3ea7431eef67ece5549f4dbc4046e79f4085b5f6
Rifaut Alexis <ralexis149@gmail.com>
2023-05-08 12:03:26 +0200

    Move save action on recipe creation

--- lib/features/recipe/recipe_creation/presentation/view/recipe_creation_page.dart
+++ lib/features/recipe/recipe_creation/presentation/view/recipe_creation_page.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// ignore: depend_on_referenced_packages
-import 'package:collection/collection.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:grocery_list/features/recipe/recipe_creation/presentation/controller/recipe_creation_controller.dart';
-import 'package:grocery_list/features/recipe/recipe_creation/presentation/widgets/ingredients_form.dart';
-import 'package:grocery_list/features/recipe/widget/keep_alive_wrapper.dart';
-import 'package:grocery_list/features/recipe/recipe_creation/presentation/widgets/preview_form.dart';
-import 'package:grocery_list/features/recipe/recipe_creation/presentation/widgets/steps_form.dart';
 import 'package:grocery_list/features/recipe/widget/recipe_scaffold.dart';
 
 class RecipeCreationPage extends GetView<RecipeCreationController> {
@@ -21,10 +14,12 @@ class RecipeCreationPage extends GetView<RecipeCreationController> {
         onPressed: () => controller.backToRecipeList(),
         icon: const Icon(Icons.arrow_back),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => controller.addRecipe(controller.formKey),
-        child: const Text('Create'),
-      ),
+      actions: [
+        IconButton(
+          onPressed: () => controller.addRecipe(controller.formKey),
+          icon: const Icon(Icons.save),
+        )
+      ],
       formKey: controller.formKey,
       tabs: controller.tabs,
     );
